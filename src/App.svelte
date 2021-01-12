7ad89dfd873da002074219f4d5efa6eb793ec349
Kurt Duyck <kurt.duyck@vives.be>
2021-01-12 15:14:07 +0100

    Add CV component - part 2
    	modified:   src/App.svelte
    	modified:   src/main.js

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,14 @@
-import CV from './CV.svelte'
 
 <script>
-	export let name;
+  import CV from './CV.svelte'
+  export let name;
 </script>
 
 <main>
+	<CV />
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+
 </main>
 
 <style>
